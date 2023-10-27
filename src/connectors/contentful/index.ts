e4f6fb34860641522759f49eee0b5cc81d00440c
Juliano Alves <contato@julianoalves.me>
2023-10-27 18:24:35 -0300

    feat: 🎸 add no-cache

--- src/connectors/contentful/index.ts
+++ src/connectors/contentful/index.ts
@@ -6,6 +6,9 @@ const access_token = process.env.NEXT_PUBLIC_DELIVERY_TOKEN;
 const client = createClient({
   space: space_id || "",
   accessToken: access_token || "",
+  headers: {
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+  },
 });
 
 export default client;
