844420c3c19fa5c639e050b49e6992c547cb262f
Juliano Alves <contato@julianoalves.me>
2023-10-27 17:58:20 -0300

    feat: 🎸 add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <></>;
+import Head from "next/head";
+import contentfulService from "@/presentation/services/contentful-pages";
+import { AboutMe } from "@/design-system/components";
+import styles from "./me.module.scss";
+import { PageHome } from "@/domain/models";
+
+export default async function Me() {
+  const { title, sections }: PageHome = await contentfulService.getHomePage();
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className={styles.main}>
+        <AboutMe {...sections[0]} />
+      </main>
+    </>
+  );
 }
