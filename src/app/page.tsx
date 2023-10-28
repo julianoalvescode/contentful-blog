05de09490b5a966dc117aea6fc70febfd05c73fb
Juliano Alves <contato@julianoalves.me>
2023-10-28 13:36:04 -0300

    feat: 🎸 refactor for SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import Head from "next/head";
-import contentfulService from "@/presentation/services/contentful-pages";
-import { AboutMe } from "@/design-system/components";
-import styles from "./me.module.scss";
-import { PageHome } from "@/domain/models";
+import { redirect } from "next/navigation";
 
-export default async function Me() {
-  const { title, sections }: PageHome = await contentfulService.getHomePage();
-
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <main className={styles.main}>
-        <AboutMe {...sections[0]} />
-      </main>
-    </>
-  );
+export default function Me() {
+  return redirect("/me");
 }
