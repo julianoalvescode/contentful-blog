0d6c5395820e71d4b1c4029b9fcc9b99c92711fa
Juliano Alves <contato@julianoalves.me>
2023-10-30 15:38:47 -0300

    feat: 🎸 add date in dd/MM/yyyy

--- src/design-system/components/Post/index.tsx
+++ src/design-system/components/Post/index.tsx
@@ -1,4 +1,4 @@
-import { humanizeDate } from "@/presentation/helpers";
+import { formatDate, humanizeDate } from "@/presentation/helpers";
 import styles from "./post.module.scss";
 
 import { PostProps } from "./types";
@@ -17,7 +17,9 @@ export function Post({
         <article className={styles.post}>
           <div className={styles["post__info-container"]}>
             <h1 className={styles["post__title"]}>{`${id}. ${title}`}</h1>
-            <p className={styles["post__date"]}>{humanizeDate(createdAt)}</p>
+            <p className={styles["post__date"]}>
+              {formatDate(createdAt)} - Postado {humanizeDate(createdAt)}
+            </p>
           </div>
           <p className={styles["post__text"]}>{resume}</p>
         </article>
--- src/presentation/helpers/index.ts
+++ src/presentation/helpers/index.ts
@@ -1,6 +1,10 @@
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export function humanizeDate(date: string) {
   return formatDistanceToNow(new Date(date), { addSuffix: true, locale: ptBR });
+}
+
+export function formatDate(date: string) {
+  return format(new Date(date), "dd/MM/yyyy", { locale: ptBR });
 }
