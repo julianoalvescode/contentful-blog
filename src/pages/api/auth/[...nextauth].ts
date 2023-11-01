c6ed5503dc7cdbfeffb0e3e04ae3c42c59edc07d
Juliano Alves <contato@julianoalves.me>
2023-11-01 16:59:23 -0300

    feat: 🎸 update env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,9 +3,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "42f66d109c7eebff001a",
-      clientSecret:
-        process.env.GITHUB_SECRET || "857060d3a84bb39c6bf90452d650b1540e188d0c",
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.NEXT_PUBLIC_SECRET,
